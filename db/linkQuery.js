0bee52b4ba502dec8685cb4a1e983d0e20ea043a
Jackson Donovan <iinventedtwitter@gmail.com>
2018-04-26 12:22:02 -0600

    can now add posts and display posts, not ordered by topic though.

--- db/linkQuery.js
+++ db/linkQuery.js
@@ -22,9 +22,27 @@ function deleteUser(username){
   return knex('users').select().where('username',username).del()
 }
 
+function addPost(obj){
+  var dt = new Date()
+  var month = dt.getMonth()+1;
+  var day = dt.getDate();
+  var year = dt.getFullYear();
+  var completeDate = month + '/' + day + '/' + year;
+
+  obj.date = completeDate;
+
+  return knex('posts').insert(obj)
+}
+
+function getPosts(){
+  return knex('posts').select()
+}
+
 module.exports = {
   addUser,
   getUsers,
   updateUser,
-  deleteUser
+  deleteUser,
+  addPost,
+  getPosts
 };
